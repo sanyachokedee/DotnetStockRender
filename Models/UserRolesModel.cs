b0e038fa1a9f6cc3b1a7f201f2de9da9505ba371
sanyachokedee <ing11819@gmail.com>
2024-06-27 10:50:35 +0700

    first commit

---
+++ Models/UserRolesModel.cs
@@ -0,0 +1,10 @@
+namespace DotnetStockAPI.Models;
+
+public class UserRolesModel
+{
+    public const string Admin = "Admin";
+    public const string Manager = "Manager";
+    public const string User = "User";
+    public const string Mod = "Mod";
+    public const string Guest = "Guest";
+}
